c9ef74a14879deae25180ce426d499b2394fd3f0
maxx5 <maxx5@asiainfo.com>
2021-03-13 00:46:43 +0800

    格式化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -118,6 +118,14 @@ module.exports = {
   },
   markdown: {
     lineNumbers: true, //代码显示行号
+    // markdown-it-anchor 的选项
+    anchor: { permalink: false },
+    // markdown-it-toc 的选项
+    toc: { includeLevel: [1, 2] },
+    extendMarkdown: md => {
+      // 使用更多的 markdown-it 插件!
+      md.use(require('markdown-it-xxx'))
+    }
   }, // 搜索设置
   search: true,
   searchMaxSuggestions: 10, // 插件
