467f0f6124657e612e0f259d838a836d4412901e
maxx5 <maxx5@asiainfo.com>
2021-03-12 23:57:22 +0800

    配置worklows

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,12 @@ module.exports = {
   port: "8080", // 生成网页端口（本地调试使用）
   title: "ma-xx Blog", // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
   description: "个人博客", // meta 中的描述文字，用于SEO
+  /* 
+   部署站点的基础路径，如果你想让你的网站部署到一个子路径下，你将需要设置它。如 GitHub pages，
+   如果你想将你的网站部署到 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/"，
+   它的值应当总是以斜杠开始，并以斜杠结束。
+  */
+  base: '/my-blog/', 
   head: [
     ["link", { rel: "icon", href: "/favicon.svg" }], //浏览器的标签栏的网页图标,基地址/docs/.vuepress/public
     [
